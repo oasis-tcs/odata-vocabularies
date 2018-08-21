ac08ede7d9c86274e2d44347666943abf034ffe7
Ralf Handl <ralf.handl@sap.com>
2018-08-21 10:38:32 +0200

    Option /JSON to transform only to CSDL JSON without generating MarkDown file

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -12,21 +12,39 @@ set CLASSPATH=%XALAN_HOME%/xalan.jar;%XALAN_HOME%/serializer.jar
 @rem  - YAJL's json_reformat from https://github.com/lloyd/yajl has been compiled and is in the PATH
 
 set done=false
+set here=%~dp0
 
-for %%F in (..\vocabularies\*.xml) do (
-	if /I [%~n1]==[%%~nF] (
-	  set done=true
-		call :process %%F
-	) else if /I [Org.OData.%1.V1]==[%%~nF] (
-	  set done=true
-		call :process %%F
-	) else if [%1]==[] (
-	  set done=true
-		call :process %%F
-	)
+if /I [%1]==[/json] (
+  set done=true
+  for %%F in (%*) do (
+    if /I [%%F]==[/json] (
+      rem skip /json
+    ) else if exist %%F (
+      call :process %%F /JSON
+    ) else (
+      echo Couldn't find %%F
+    )
+  )
+) else (
+  for %%F in (%here%..\vocabularies\*.xml) do (
+    if /I [%~n1]==[%%~nF] (
+      set done=true
+      call :process %%F 
+    ) else if /I [Org.OData.%1.V1]==[%%~nF] (
+      set done=true
+      call :process %%F 
+    ) else if [%1]==[] (
+      set done=true
+      call :process %%F 
+    )
+  )
 )
 
-if %done%==false echo Don't know how to %~n0 %1
+if %done%==false (
+  echo Don't know how to %~n0 %1
+  echo:
+  echo Usage: transform [/JSON] [FILE]...
+)
 
 endlocal
 exit /b
@@ -34,16 +52,19 @@ exit /b
 
 :process
   echo %~n1
+  set FILENAME=%~dpn1
+
+  if not "%2" == "/JSON" (
+    java.exe org.apache.xalan.xslt.Process -L -XSL %here%Vocab-to-MarkDown.xsl -IN %1 -OUT %FILENAME%.md
+    git.exe -C %~dp1 --no-pager diff --color-words="[^[:space:]]|^(#L[0-9]+)" %FILENAME%.md 2>nul
+  )
   
-  java.exe org.apache.xalan.xslt.Process -L -XSL Vocab-to-MarkDown.xsl -IN %1 -OUT ..\vocabularies\%~n1.md
-  git.exe --no-pager diff --color-words="[^[:space:]]|^(#L[0-9]+)" ..\vocabularies\%~n1.md
-  
-  java.exe org.apache.xalan.xslt.Process -L -XSL V4-CSDL-normalize-Target.xsl -IN %1 -OUT %~n1.normalized.xml
-  java.exe org.apache.xalan.xslt.Process -L -XSL V4-CSDL-to-JSON.xsl -IN %~n1.normalized.xml -OUT %~n1.json
-  json_reformat.exe < %~n1.json > ..\vocabularies\%~n1.json
+  java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-normalize-Target.xsl -IN %1 -OUT %FILENAME%.normalized.xml
+  java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-to-JSON.xsl -IN %FILENAME%.normalized.xml -OUT %FILENAME%.tmp.json
+  json_reformat.exe < %FILENAME%.tmp.json > %FILENAME%.json
   if not errorlevel 1 (
-    del %~n1.normalized.xml %~n1.json
-    git.exe --no-pager diff ..\vocabularies\%~n1.json  
+    del %FILENAME%.normalized.xml %FILENAME%.tmp.json
+    git.exe -C %~dp1 --no-pager diff %FILENAME%.json  2>nul
   )
 
 exit /b
