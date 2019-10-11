5e6a0c3820decd6174ce2d97ede638948036b517
Ralf Handl <ralf.handl@sap.com>
2019-10-11 15:52:39 +0200

    Headline

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -4,20 +4,21 @@ const fs = require('fs');
 //TODO:
 // everything
 
+const csdl = require('odata-csdl');
 const lib = require('../lib/csdl2markdown');
 
-const example1 = require('../vocabularies/Org.OData.Aggregation.V1.json');
+const example1 = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.xml'));
 const result1 = fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.md', 'utf8');
 
 describe('Examples', function () {
 
     it('Aggregation', function () {
-        const markdown = lib.csdl2markdown(example1);
+        const markdown = lib.csdl2markdown('Org.OData.Aggregation.V1.xml',example1);
         check(markdown, result1);
     })
 
 })
 
 function check(actual, expected) {
-    assert.deepStrictEqual(actual.split(/\r\n|\r|\n/g), expected.split(/\r\n|\r|\n/g), 'Markdown');
+    assert.deepStrictEqual(actual, expected.split(/\r\n|\r|\n/g), 'Markdown');
 }
