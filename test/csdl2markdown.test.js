f28f0e380a034d4c7eefd20998bf152daa6c2ede
Ralf Handl <ralf.handl@sap.com>
2019-10-12 16:27:52 +0200

    Prepared next test case Capabilities

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -1,28 +1,26 @@
 const assert = require('assert');
 const fs = require('fs');
 
-//TODO:
-// everything
-
 const csdl = require('odata-csdl');
 const lib = require('../lib/csdl2markdown');
 
-const example1 = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.xml'), true);
-const result1 = fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.md', 'utf8');
+//TODO: Core, Measures, Repeatability, Temporal, Validation
+const vocabularies = ['Aggregation', 'Authorization', 'Capabilities'];
+const input = {};
+const expected = {};
 
-const example2 = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.Authorization.V1.xml'), true);
-const result2 = fs.readFileSync('vocabularies/Org.OData.Authorization.V1.md', 'utf8');
+vocabularies.forEach(v => {
+    input[v] = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.xml'), true);
+    expected[v] = fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.md', 'utf8');
+})
 
 describe('Examples', function () {
 
-    it('Aggregation', function () {
-        const markdown = lib.csdl2markdown('Org.OData.Aggregation.V1.xml', example1);
-        check(markdown, result1);
-    })
-
-    it('Authorization', function () {
-        const markdown = lib.csdl2markdown('Org.OData.Authorization.V1.xml', example2);
-        check(markdown, result2);
+    vocabularies.forEach(v => {
+        it(v, function () {
+            const markdown = lib.csdl2markdown('Org.OData.' + v + '.V1.xml', input[v]);
+            check(markdown, expected[v]);
+        })
     })
 
 })
