47ba2392b6c2e5b4e90c759bf22b6af0d2c90978
Ralf Handl <ralf.handl@sap.com>
2019-10-12 14:49:52 +0200

    CLI fixed Next test case added

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -10,11 +10,19 @@ const lib = require('../lib/csdl2markdown');
 const example1 = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.xml'), true);
 const result1 = fs.readFileSync('vocabularies/Org.OData.Aggregation.V1.md', 'utf8');
 
+const example2 = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.Authorization.V1.xml'), true);
+const result2 = fs.readFileSync('vocabularies/Org.OData.Authorization.V1.md', 'utf8');
+
 describe('Examples', function () {
 
     it('Aggregation', function () {
         const markdown = lib.csdl2markdown('Org.OData.Aggregation.V1.xml', example1);
         check(markdown, result1);
+    })
+
+    it('Authorization', function () {
+        const markdown = lib.csdl2markdown('Org.OData.Authorization.V1.xml', example2);
+        check(markdown, result2);
     })
 
 })
