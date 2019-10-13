78df574a9d269bef8952975e9b308f83d7e1fa96
Ralf Handl <ralf.handl@sap.com>
2019-10-13 16:13:46 +0200

    Interpret Core.Deprecated

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 const csdl = require('odata-csdl');
 const lib = require('../lib/csdl2markdown');
 
-const vocabularies = ['Aggregation', 'Authorization', 'Capabilities', 'Core', 'Measures', 'Repeatability', 'Temporal', 'Validation'];
 const input = {};
 const expected = {};
+
+const vocabularies = ['Aggregation', 'Authorization', 'Capabilities', 'Core', 'Measures', 'Repeatability', 'Temporal', 'Validation'];
 
 vocabularies.forEach(v => {
     input[v] = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.xml'), true);
     expected[v] = fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.md', 'utf8');
 })
 
-describe('Examples', function () {
+describe('OASIS Vocabularies', function () {
 
     vocabularies.forEach(v => {
         it(v, function () {
@@ -23,6 +24,26 @@ describe('Examples', function () {
     })
 
 })
+
+//TODO: remove from here
+// const sapVocabs = ['Analytics', 'CodeList', 'Common', 'Communication', 'Hierarchy', 'PersonalData', 'Session', 'UI'];
+
+// sapVocabs.forEach(v => {
+//     input[v] = csdl.xml2json(fs.readFileSync('../vocabularies/' + v + '.xml'), true);
+//     expected[v] = fs.readFileSync('../vocabularies/' + v + '.md', 'utf8');
+// })
+
+// describe('SAP Vocabularies', function () {
+
+//     sapVocabs.forEach(v => {
+//         it(v, function () {
+//             const markdown = lib.csdl2markdown(v + '.xml', input[v]);
+//             check(markdown, expected[v]);
+//         })
+//     })
+
+// })
+//TODO: remove up to here
 
 function check(actual, expected) {
     assert.deepStrictEqual(actual, expected.split(/\r\n|\r|\n/g), 'Markdown');
