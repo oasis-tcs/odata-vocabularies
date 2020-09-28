a1c700f70faf6dc26c7005186b84e6372e245492
Ralf Handl <ralf.handl@sap.com>
2020-09-28 09:05:58 +0200

    Refactored

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -10,20 +10,23 @@ const expected = {};
 const vocabularies = ['Aggregation', 'Authorization', 'Capabilities', 'Core', 'Measures', 'Repeatability', 'Temporal', 'Validation'];
 
 vocabularies.forEach(v => {
-    input[v] = csdl.xml2json(fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.xml'), true);
-    expected[v] = fs.readFileSync('vocabularies/Org.OData.' + v + '.V1.md', 'utf8');
+    const vocab = `Org.OData.${v}.V1`;
+    input[`${vocab}.xml`] = csdl.xml2json(fs.readFileSync(`vocabularies/${vocab}.xml`), true);
+    expected[v] = fs.readFileSync(`vocabularies/${vocab}.md`, 'utf8');
 })
 
 describe('OASIS Vocabularies', function () {
 
     it('Validation without potentially referenced vocabularies', function () {
-        const markdown = lib.csdl2markdown('Org.OData.Validation.V1.xml', input.Validation);
+        const filename = 'Org.OData.Validation.V1.xml'
+        const markdown = lib.csdl2markdown(filename, input[filename]);
         check(markdown, expected.Validation);
     })
 
     vocabularies.forEach(v => {
         it(v, function () {
-            const markdown = lib.csdl2markdown('Org.OData.' + v + '.V1.xml', input[v], input);
+            const filename = `Org.OData.${v}.V1.xml`;
+            const markdown = lib.csdl2markdown(filename, input[filename], input);
             check(markdown, expected[v]);
         })
     })
