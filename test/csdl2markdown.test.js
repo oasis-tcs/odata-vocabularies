9cc37cf8af12dd0d79b954cd98f4a6c82d9ce6bb
Ralf Handl <ralf.handl@sap.com>
2020-10-14 11:32:52 +0200

    More test cases

--- test/csdl2markdown.test.js
+++ test/csdl2markdown.test.js
@@ -33,6 +33,127 @@ describe('OASIS Vocabularies', function () {
 
 })
 
+describe('Non-OASIS Vocabularies', function () {
+
+    it('Non-OASIS Vocabulary referencing an OASIS Vocabulary', function () {
+        const filename = 'Other.xml'
+        const vocabulary = {
+            $Version: '4.01',
+            $Reference: {
+                'https://oasis-tcs.github.io/odata-vocabularies/vocabularies/Org.OData.Core.V1.json': {
+                    $Include: [{
+                        $Namespace: 'Org.OData.Core.V1'
+                    }]
+                },
+                'Another.xml': {
+                    $Include: [{
+                        $Namespace: 'com.acme.Another.v42',
+                        $Alias: 'Another'
+                    }]
+                }
+            },
+            'com.acme.Other.v42': {
+                $Alias: 'Other',
+                '@Org.OData.Core.V1.Description': 'Other terms',
+                Foo: {
+                    $Kind: 'Term',
+                    $Type: 'Org.OData.Core.V1.Tag',
+                    '@Org.OData.Core.V1.Revisions': [
+                        {
+                            Kind: 'Deprecated',
+                            Description: 'Use term `Foo` from the OASIS Core vocabulary instead'
+                        }
+                    ]
+                },
+                Bar: {
+                    $Kind: 'Term',
+                    $Type: 'Another.Type'
+                }
+            }
+        }
+        const expectedMarkdown = [
+            '# Other Vocabulary',
+            '**Namespace: [com.acme.Other.v42](Other.xml)**',
+            '',
+            'Other terms',
+            '',
+            '',
+            '## Terms',
+            '',
+            'Term|Type|Description',
+            ':---|:---|:----------',
+            'Foo *(Deprecated)*|[Tag](https://github.com/oasis-tcs/odata-vocabularies/blob/master/vocabularies/Org.OData.Core.V1.md#Tag)|<a name=\"Foo\"></a>Use term `Foo` from the OASIS Core vocabulary instead',
+            'Bar|[Type](Another.md#Type)|<a name=\"Bar\"></a>',
+            ''
+        ]
+        const markdown = lib.csdl2markdown(filename, vocabulary);
+        assert.deepStrictEqual(markdown, expectedMarkdown);
+    })
+
+    it('No references to other vocabularies', function () {
+        const filename = 'Selfcontained.xml'
+        const vocabulary = {
+            $Version: '4.01',
+            'com.acme.Selfcontained.v0': {
+                Foo: {
+                    $Kind: 'Term',
+                    $Type: 'com.acme.Selfcontained.v0.Bar'
+                }
+            }
+        }
+        const expectedMarkdown = [
+            '# Selfcontained Vocabulary',
+            '**Namespace: [com.acme.Selfcontained.v0](Selfcontained.xml)**',
+            '',
+            undefined, // No Core.Description for vocabulary
+            '',
+            '',
+            '## Terms',
+            '',
+            'Term|Type|Description',
+            ':---|:---|:----------',
+            'Foo|[Bar](#Bar)|<a name="Foo"></a>',
+            ''
+        ]
+        const markdown = lib.csdl2markdown(filename, vocabulary);
+        assert.deepStrictEqual(markdown, expectedMarkdown);
+    })
+
+    it('Reference without Include', function () {
+        const filename = 'Funny.json'
+        const vocabulary = {
+            $Version: '4.01',
+            $Reference: {
+                '': {}
+            },
+            'Funny.v0': {
+                Foo: {
+                    $Kind: 'Term',
+                    $Type: 'Edm.String',
+                    '@com.sap.vocabularies.Common.v1.Experimental': true
+                }
+            }
+        }
+        const expectedMarkdown = [
+            '# Funny Vocabulary',
+            '**Namespace: [Funny.v0](Funny.json)**',
+            '',
+            undefined, // No Core.Description for vocabulary
+            '',
+            '',
+            '## Terms',
+            '',
+            'Term|Type|Description',
+            ':---|:---|:----------',
+            'Foo *([Experimental](Common.md#Experimental))*|String|<a name="Foo"></a>',
+            ''
+        ]
+        const markdown = lib.csdl2markdown(filename, vocabulary);
+        assert.deepStrictEqual(markdown, expectedMarkdown);
+    })
+
+})
+
 function check(actual, expected) {
     assert.deepStrictEqual(actual, expected.split(/\r\n|\r|\n/g), 'Markdown');
 }
