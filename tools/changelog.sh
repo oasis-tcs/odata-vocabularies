84e53b081b9d01db9863e5b8fa72ad9e87d23bcb
D024504 <heiko.theissen@sap.com>
2021-09-23 11:45:15 +0200

    determination of remote URL

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,7 +1,8 @@
+remote=$(git remote show origin | grep -P -o "(?<=Push  URL: ).*")
 folder=$(mktemp -d)
 git log | node tools/changelog >$folder/CHANGELOG.md
 cd $folder
 git init -b main
 git add .
 git commit -m "changelog"
-git push --force git@github.com:oasis-tcs/odata-vocabularies.git main:changelog
+git push --force $remote main:changelog
