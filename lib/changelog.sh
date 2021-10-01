f539a8a2266b89d67195c699db021b520f11ef42
D024504 <heiko.theissen@sap.com>
2021-10-01 14:13:14 +0200

    maintain CHANGELOG.md in main branch

--- lib/changelog.sh
+++ lib/changelog.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-remote=$(git remote show origin | grep -P -o "(?<=Push  URL: ).*")
-folder=$(mktemp -d)
-node lib/changelog >$folder/CHANGELOG.md
-cd $folder
-git init -b main
-git add .
-git commit -m "changelog"
-git push --force $remote main:changelog
-
-# Alternative
-# node lib/changelog >CHANGELOG.md
-# git add CHANGELOG.md
-# git diff-index --quiet HEAD || git commit -m "changelog"
-# git push
+node lib/changelog >CHANGELOG.md
+git add CHANGELOG.md
+git diff-index --quiet HEAD || git commit -m "changelog"
+git push
