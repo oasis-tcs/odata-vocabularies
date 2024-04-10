47b31bf091f6d45500d8bfe0033e9e5968610fe0
Heiko Theißen <heiko.theissen@sap.com>
2024-04-10 17:16:56 +0200

    Build github.io pages with pandoc instead of Jekyll (#248)

---
+++ test/pandoc.test.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+const fs = require("fs");
+const pandoc = require("../lib/pandoc");
+const { PassThrough } = require("stream");
+
+describe("OASIS voc build", function () {
+  it("Pandoc", function () {
+    var html = "";
+    pandoc(
+      {
+        stdin: fs.createReadStream(`${__dirname}/test-data/pandoc.md`),
+        stdout: new PassThrough()
+          .on("data", function (chunk) {
+            html += chunk.toString();
+          })
+          .on("end", function () {
+            assert.deepStrictEqual(
+              html.split(/\r\n|\r|\n/),
+              fs
+                .readFileSync(`${__dirname}/test-data/pandoc.html`)
+                .toString()
+                .split(/\r\n|\r|\n/),
+            );
+          }),
+      },
+      {
+        "-c": "style.css",
+        "-F": `${__dirname}/../lib/md2html.js`,
+        "-M": "title=Test",
+        "--template": `${__dirname}/../assets/template`,
+      },
+    );
+  });
+});
